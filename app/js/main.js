efa8b780c17411f7ae75d076416dd9884523ce2f
TanyaSummer <89299874440@mail.ru>
2021-10-11 17:50:43 +0300

    add slick slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
 $(function(){
-  // $('body').hide();
+
+  
+
+  $('.rate__item').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerPadding: '22%',
+    // autoplay: true,
+    // autoplaySpeed: 9000,
+    prevArrow:'<button type="button" class="slick-prev"><svg width="12" height="13" viewBox="0 0 12 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.529 5.59101H2.82398L6.82398 1.59101L5.80698 0.584015L0.0839844 6.30701L5.80698 12.03L6.81598 11.021L2.82398 7.02102H11.53L11.529 5.59101Z" fill="#E41C4C"/></svg></button>',
+    nextArrow:'<button type="button" class="slick-next"><svg width="12" height="13" viewBox="0 0 12 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.80698 0.584015L4.79798 1.59301L8.78998 5.59301H0.0839844V7.02401H8.78998L4.79798 11.024L5.80698 12.033L11.53 6.31001L5.80698 0.584015Z" fill="#E41C4C"/></svg></button>',
+  });
+
 });
